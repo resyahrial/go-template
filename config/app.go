76cb86ddd5247a0b81e75a13cefc3672c70523b7
resyahrial <syahrialrangga11@gmail.com>
2022-12-02 14:35:55 +0700

    add air toml setup

--- config/app.go
+++ config/app.go
@@ -4,7 +4,6 @@ type AppConfig struct {
 	Name          string `yaml:"name"`
 	Version       string `yaml:"version"`
 	Environment   string `yaml:"-"`
-	LogLevel      int    `yaml:"loglevel"`
 	DebugMode     bool   `yaml:"debug"`
 	ServerAppHost string `yaml:"host"`
 	ServerAppPort string `yaml:"port"`
